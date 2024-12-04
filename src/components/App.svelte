808280f3e73d0c2bbd2b75efe7bd9e8ba32d19ef
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-12-03 22:47:33 -0600

    add store and components of svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,5 +1,22 @@
 <script>
-  import { Progressbar } from "flowbite-svelte";
+  import { Progressbar, Alert } from "flowbite-svelte";
+  import { APP_STATUS, appStatusCheck } from "../store";
 </script>
 
-<Progressbar progress="50" />
+{#if $appStatusCheck === APP_STATUS.INIT}
+  <p>La pagina inicial esta cargando</p>
+{:else if $appStatusCheck === APP_STATUS.LOADING}
+  <p>La pagina esta cargando</p>
+{:else if $appStatusCheck === APP_STATUS.CHAT_MODE}
+  <p>La pagina esta en modo chat</p>
+{:else if $appStatusCheck === APP_STATUS.ERROR}
+  <Alert color="red">
+    <span class="font-medium">Algo malo ha pasado</span>
+    Hubo un error en la app.
+  </Alert>
+{/if}
+
+<Alert color="red">
+  <span class="font-medium">Danger alert!</span>
+  Change a few things up and try submitting again.
+</Alert>
