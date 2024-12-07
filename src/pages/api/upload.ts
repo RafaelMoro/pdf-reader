c2411ce3800109ec84091eb17c8f725cb64a8ca9
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-12-06 23:38:52 -0600

    Add ocr into upload and then add logic to save pdf into text file meanwhile

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from "astro";
 // import { createReadStream } from "streamifier";
+import fs from 'node:fs/promises'
+import path from "node:path";
 import { v2 as cloudinary, type UploadApiResponse } from "cloudinary";
 
 cloudinary.config({ 
@@ -8,8 +10,11 @@ cloudinary.config({
   api_secret: import.meta.env.CLOUDINARY_SECRET // Click 'View API Keys' above to copy your API secret
 });
 
+const outputDir = path.join(process.cwd(), 'public/text')
+
 const uploadStream = async (buffer: Uint8Array, options: {
-  folder: string
+  folder: string;
+  ocr?: string;
 }): Promise<UploadApiResponse > => {
   try {
     return new Promise((resolve, reject) => {
@@ -38,12 +43,30 @@ export const POST: APIRoute = async ({ request }: { request: Request }) => {
     const unit8Array = new Uint8Array(arrayBuffer);
     const result = await uploadStream(unit8Array, {
       folder: "pdf",
+      ocr: 'adv_ocr'
     });
     const {
       asset_id: id,
       secure_url: url,
       pages,
+      info
     } = result;
+
+    // Advanced OCR from cloudinary
+    // Docs: https://cloudinary.com/documentation/ocr_text_detection_and_extraction_addon?customer_external_id=aba5f859d76dfc7d689eb19a539a7d&frameless=1
+    const data = info?.ocr?.adv_ocr?.data
+    const text = data.map((blocks: { textAnnotations: { description: string }[] }) => {
+      const annotations = blocks['textAnnotations'] ?? {}
+      const first = annotations[0] ?? {}
+      const content = first['description'] ?? ''
+      return content.trim()
+    }).filter(Boolean).join('\n')
+
+    // Deberiamos de meter la info en una base de datos
+    // O mejor todavia, en un vector y hacer los embeddings
+    // Como minimo, lo deberiamos de subir a un S3 privado
+    fs.writeFile(path.join(outputDir, `${id}.txt`), text, 'utf8')
+
     return new Response(JSON.stringify({
       id,
       url,
